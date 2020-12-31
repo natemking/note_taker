37e51133da359eec71ccf05abf526f876f9f15f7
Nate M King <72567521+natemking@users.noreply.github.com>
2020-12-31 10:31:28 -0500

    Updated CSS styling for hover on .navbar-brand. Updated port variable in server.js to dymanic port w/ 3000 fallback for Heroku deployment

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 //*** Express app ***//
 //===================//
 const app = express();
-const PORT = 3000;
+const port = process.env.PORT || 3000
 
 //*** Directories ***//
 //===================//
@@ -36,7 +36,7 @@ app.use((req, res) =>{
 
 //*** Listener ***//
 //================//
-app.listen(PORT, () => {
-  console.log(`Server listening on port:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server listening on port:${port}`);
 });
 
