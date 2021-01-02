06ba5d9f402bece8b352cec5f314b9ac79c9d58d
Nate M King <72567521+natemking@users.noreply.github.com>
2021-01-02 15:35:50 -0500

    Created a writeToFile module and required in server.js. Updated POST & GET to use this new module.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
+const writeToFile = require('./lib/writeToFile')
 
 
 //*** Express app ***//
@@ -21,10 +22,11 @@ const notes = path.join(PUBLIC_DIR, 'notes.html');
 const db = path.join(DB_DIR, 'db.json');
 const data = JSON.parse(fs.readFileSync(db));
 
+
 //*** Middleware ***//
 //==================//
 app.use(express.static(PUBLIC_DIR));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //*** Routes ***//
@@ -40,62 +42,60 @@ app.get('/notes', (req, res) => {
 app.route('/api/notes/:id?')
     .get((req, res) => {
         const id = parseFloat(req.params.id);
-        
-        if(id){
-           for(let i = 0; i < data.length; i++ ){
-               if (id === data[i].id){
-                   res.json(data[i]);
-                   return;
-               }
-           }
-           res.send('No data found')
-        }else{
+
+        if (id) {
+            for (let i = 0; i < data.length; i++) {
+                if (id === data[i].id) {
+                    res.json(data[i]);
+                    return;
+                }
+            }
+            res.send('No data found')
+        } else {
             res.sendFile(db);
-        }  
+        }
     })
     .post((req, res) => {
         //push user data to db.json array
         data.push(req.body);
-
         //add an id to each note
         for (let i = 0; i < data.length; i++) {
             data[i].id = i + 1;
         }
         //Write file w/ new user data
-        fs.writeFile(db, JSON.stringify(data), (err) => {
-          err ? console.error(err) : console.log("User data added");
-        });
+        writeToFile(db, JSON.stringify(data), 'posted');
+
+        res.end();
     })
-    .delete((req, res) => {
-        
+    .delete((req, res) => {  
+
         const id = parseFloat(req.params.id);
 
         //Remove the note from data array
         for (let i = 0; i < data.length; i++) {
-            if (id === data[i].id){
+            if (id === data[i].id) {
                 data.splice(i, 1);
             }
         }
-        
+
         //update the notes id#
         for (let i = 0; i < data.length; i++) {
             data[i].id = i + 1;
         }
 
-        //Write file w/ new user data
-        fs.writeFile(db, JSON.stringify(data), (err) => {
-            err ? console.error(err) : console.log("User data added");
-          });
-      
+        //Write file w/ user data deleted
+        writeToFile(db, JSON.stringify(data), 'deleted');
+
+        res.end();
     });
 
 //404
-app.use((req, res) =>{
+app.use((req, res) => {
     res.sendFile(`${PUBLIC_DIR}/404.html`)
 });
 
 //*** Listener ***//
 //================//
 app.listen(PORT, () => {
-  console.log(`Server listening on PORT:${PORT}`);
+    console.log(`Server listening on PORT:${PORT}`);
 });
