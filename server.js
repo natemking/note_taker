bf70f8ab3e6c05d1f1252424f7e3827864ee550a
Nate M King <72567521+natemking@users.noreply.github.com>
2021-01-02 17:45:53 -0500

    Split off 2 functions that are used more than once into their own modules in a ./lib folder

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-const writeToFile = require('./lib/writeToFile')
-
 
 //*** Express app ***//
 //===================//
@@ -17,11 +15,16 @@ const PORT = process.env.PORT || 3000
 //===================//
 const PUBLIC_DIR = path.resolve(__dirname, './public');
 const DB_DIR = path.resolve(__dirname, './db');
+const LIB_DIR = path.resolve(__dirname, './lib')
 const index = path.join(PUBLIC_DIR, 'index.html');
 const notes = path.join(PUBLIC_DIR, 'notes.html');
 const db = path.join(DB_DIR, 'db.json');
 const data = JSON.parse(fs.readFileSync(db));
 
+//*** Custom Modules ***//
+//======================//
+const writeToFile = require(`${LIB_DIR}/writeToFile`)
+const addID = require(`${LIB_DIR}/addID`);
 
 //*** Middleware ***//
 //==================//
@@ -39,49 +42,42 @@ app.get('/notes', (req, res) => {
     res.sendFile(notes);
 });
 
+
 app.route('/api/notes/:id?')
     .get((req, res) => {
+        //Filter and display the notes per their id
         const id = parseFloat(req.params.id);
-        if(id){
+        if (id) {
             const arr = data.filter((note) => {
-                return id === note.id
+                return id === note.id;
             });
             res.json(arr);
-        }else{
+        } else {
             res.sendFile(db);
         }
+        return id;
     })
     .post((req, res) => {
         //push user data to db.json array
         data.push(req.body);
         //add an id to each note
-        for (let i = 0; i < data.length; i++) {
-            data[i].id = i + 1;
-        }
+        addID(data);
         //Write file w/ new user data
         writeToFile(db, JSON.stringify(data), 'posted');
-
         res.end();
     })
-    .delete((req, res) => {  
-
+    .delete((req, res) => {
         const id = parseFloat(req.params.id);
-
         //Remove the note from data array
         for (let i = 0; i < data.length; i++) {
             if (id === data[i].id) {
                 data.splice(i, 1);
             }
         }
-
         //update the notes id#
-        for (let i = 0; i < data.length; i++) {
-            data[i].id = i + 1;
-        }
-
+        addID(data);
         //Write file w/ user data deleted
         writeToFile(db, JSON.stringify(data), 'deleted');
-
         res.end();
     });
 
