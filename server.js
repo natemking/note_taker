cc307e9a2ae8a575774f15a77d90e9bd84c74d3f
Nate M King <72567521+natemking@users.noreply.github.com>
2021-01-04 15:27:03 -0500

    Moved API routes above HTML routes and refactored .filter method in api .get

--- server.js
+++ server.js
@@ -34,49 +34,53 @@ app.use(bodyParser.json());
 
 //*** Routes ***//
 //==============//
+
+//API route
+app.route('/api/notes/:id?')
+.get((req, res) => {
+    const id = parseFloat(req.params.id);
+    //Filter and display notes per their id if specific url is requested else show all notes
+    if (id) {
+        res.json(data.filter(note => id === note.id ));
+    } else {
+        res.sendFile(db);
+    }
+})
+.post((req, res) => {
+    //push user data to db.json array
+    data.push(req.body);
+    //add an id to each note
+    addID(data);
+    //Write file w/ new user data
+    writeToFile(db, JSON.stringify(data), 'posted');
+    res.end();
+})
+.delete((req, res) => {
+    const id = parseFloat(req.params.id);
+    //Remove the note from data array
+    for (let i = 0; i < data.length; i++) {
+        if (id === data[i].id) {
+            data.splice(i, 1);
+        }
+    }
+    //update the notes id#
+    addID(data);
+    //Write file w/ user data deleted
+    writeToFile(db, JSON.stringify(data), 'deleted');
+    res.end();
+});
+
+//Root route
 app.get('/', (req, res) => {
     res.sendFile(index);
 });
 
+//Notes route
 app.get('/notes', (req, res) => {
     res.sendFile(notes);
 });
 
-app.route('/api/notes/:id?')
-    .get((req, res) => {
-        //Filter and display the notes per their id if specific url is requested
-        const id = parseFloat(req.params.id);
-        if (id) {
-            res.json(data.filter((note) => id === note.id ));
-        } else {
-            res.sendFile(db);
-        }
-    })
-    .post((req, res) => {
-        //push user data to db.json array
-        data.push(req.body);
-        //add an id to each note
-        addID(data);
-        //Write file w/ new user data
-        writeToFile(db, JSON.stringify(data), 'posted');
-        res.end();
-    })
-    .delete((req, res) => {
-        const id = parseFloat(req.params.id);
-        //Remove the note from data array
-        for (let i = 0; i < data.length; i++) {
-            if (id === data[i].id) {
-                data.splice(i, 1);
-            }
-        }
-        //update the notes id#
-        addID(data);
-        //Write file w/ user data deleted
-        writeToFile(db, JSON.stringify(data), 'deleted');
-        res.end();
-    });
-
-//Catch all route that sends user to index.html
+//Catch all route to redirect to index.html
 app.all('*',(req, res) => {
     res.redirect('/');
 });
